6939cf5ff42ea97a9de53f492e4d2838bc737c59
Kartik Shankar <78061626+kartikshankar-nyc@users.noreply.github.com>
2025-04-21 18:58:54 -0400

    Update generateSVG.js

--- generateSVG.js
+++ generateSVG.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
-// Modify the lines to match your dynamic content
+// Adjusted SVG content with increased dimensions and improved styling for scrolling
 const svgContent = `
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="400">
+<svg xmlns="http://www.w3.org/2000/svg" width="800" height="200">
     <text x="50%" y="50%" fill="#0969DA" font-size="32" font-family="Montserrat" font-weight="600" alignment-baseline="middle" text-anchor="middle">
         Technologist, Engineering Leader, Tinkerer, Lifelong Learner
     </text>
 </svg>
 `;
 
-// Save the SVG to a file
+// Save the updated SVG to a file
 fs.writeFileSync('dynamic-typing.svg', svgContent);
 console.log('SVG file generated successfully!');
