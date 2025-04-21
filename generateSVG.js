05e0bd412a9415e6f49abfa98eac5a79679e130b
Kartik Shankar <78061626+kartikshankar-nyc@users.noreply.github.com>
2025-04-21 18:39:49 -0400

    Create generateSVG.js

---
+++ generateSVG.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// Modify the lines to match your dynamic content
+const svgContent = `
+<svg xmlns="http://www.w3.org/2000/svg" width="600" height="100">
+    <text x="50%" y="50%" fill="#0969DA" font-size="32" font-family="Montserrat" font-weight="600" alignment-baseline="middle" text-anchor="middle">
+        Technologist, Engineering Leader, Tinkerer, Lifelong Learner
+    </text>
+</svg>
+`;
+
+// Save the SVG to a file
+fs.writeFileSync('dynamic-typing.svg', svgContent);
+console.log('SVG file generated successfully!');
