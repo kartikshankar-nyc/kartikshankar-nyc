3ec52a87e5f31d03eaae87b39c984f8957d0f11e
Kartik Shankar <78061626+kartikshankar-nyc@users.noreply.github.com>
2025-04-21 18:57:08 -0400

    Update generateSVG.js

--- generateSVG.js
+++ generateSVG.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // Modify the lines to match your dynamic content
 const svgContent = `
-<svg xmlns="http://www.w3.org/2000/svg" width="600" height="100">
+<svg xmlns="http://www.w3.org/2000/svg" width="800" height="400">
     <text x="50%" y="50%" fill="#0969DA" font-size="32" font-family="Montserrat" font-weight="600" alignment-baseline="middle" text-anchor="middle">
         Technologist, Engineering Leader, Tinkerer, Lifelong Learner
     </text>
